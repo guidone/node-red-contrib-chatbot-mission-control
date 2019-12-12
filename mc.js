ab8e4c38d4ab7ff60720dea377d67f8f4874a273
Guidone <guido.bellomo@gmail.com>
2019-12-12 11:28:13 +0100

    initial commit

---
+++ mc.js
@@ -0,0 +1,24 @@
+const serveStatic = require('serve-static');
+const path = require('path');
+
+let initialized = false;
+
+module.exports = function(RED) {
+  if (!initialized) {
+    initialized = true;
+    bootstrap(RED.server, RED.httpNode || RED.httpAdmin, RED.log, RED.settings);
+  }
+  // exposed methods
+  return {
+
+  };
+}
+
+// from https://github.com/node-red/node-red-dashboard/blob/63da162998c421b43a6e5ebf447ed90e04040aa3/ui.js#L309
+function bootstrap(server, app, log, redSettings) {
+
+  log.info('RedBot Mission Control 0.0.1');
+
+  app.use('/mc', serveStatic(path.join(__dirname, 'src')));
+
+};
